f029aa0ff711f866045245c9eea06d9f47b804c8
tschmidtb51 <65305130+tschmidtb51@users.noreply.github.com>
2023-06-30 10:33:05 +0200

    chore: update version to 1.3.8

--- backend/lib/app.js
+++ backend/lib/app.js
@@ -9,7 +9,7 @@ export default async function(fastify) {
     openapi: {
       info: {
         title: 'CSAF Validator Service',
-        version: '1.3.7',
+        version: '1.3.8',
         description:
           'This is a service to validate documents against the CSAF standard.',
         contact: {
